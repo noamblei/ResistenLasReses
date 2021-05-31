5c70bf0664d70bc3ce4064c8306a8eeb942c9206
Noam <noamblei@gmail.com>
2021-05-31 02:09:26 -0300

    Code exported

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Se vienen cosas facheras...
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Rick Rolled
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
