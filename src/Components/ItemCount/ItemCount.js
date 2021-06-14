6c1e1bf75fd150cd6f1bdad6527367d4df70a76b
Noam <noamblei@gmail.com>
2021-06-14 01:49:39 -0300

    OnAdd implemented

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import "./ItemCount.css"
 
-export function ItemCount (props) {
-    const [count,setCount] = useState(0);
-    return(
-        <div className="input-group mb-3">
-          <button className="btn btn-outline-info" type="button" onClick={() => count > 0 ? setCount(count - 1) : alert("No hay más items para descartar")}>-</button>
-          <input disabled type="number" className="item-counter" value={count} aria-label="(Item Count)"/>
-          <button className="btn btn-outline-info" type="button" onClick={() => (props.stock > count) ? setCount(count + 1) : alert("No contamos con más cantidad de este producto en el momento")}>+</button>
-        </div> 
-    )
+export function ItemCount ({stock, onAdd, onSubstract}) {
+  const [count,setCount] = useState(1);
+  return(
+      <div className="input-group mb-3">
+        <button className="btn btn-outline-info" type="button" onClick={count > 1 ? () => {setCount(count - 1); onSubstract()} : () => alert("No puedes descartar más items")}>-</button>
+        <input disabled type="number" className="item-counter" value={count} aria-label="(Item Count)"/>
+        <button className="btn btn-outline-info" type="button" onClick={(stock > count) ? () => {setCount(count+1); onAdd()} : () => {alert("No contamos con más cantidad de este producto en el momento")}}>+</button>
+      </div> 
+  );
 }
