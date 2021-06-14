4881487302eded0e017181053b50e0a3d27c8327
Noam <noamblei@gmail.com>
2021-06-14 02:03:35 -0300

    OnAdd implemented

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -4,10 +4,10 @@ import "./ItemCount.css"
 export function ItemCount ({stock, onAdd, onSubstract}) {
   const [count,setCount] = useState(1);
   return(
-      <div className="input-group mb-3">
-        <button className="btn btn-outline-info" type="button" onClick={count > 1 ? () => {setCount(count - 1); onSubstract()} : () => alert("No puedes descartar más items")}>-</button>
-        <input disabled type="number" className="item-counter" value={count} aria-label="(Item Count)"/>
-        <button className="btn btn-outline-info" type="button" onClick={(stock > count) ? () => {setCount(count+1); onAdd()} : () => {alert("No contamos con más cantidad de este producto en el momento")}}>+</button>
-      </div> 
-  );
+    <div className="input-group mb-3">
+      <button className="btn btn-outline-info" type="button" onClick={count > 1 ? () => {setCount(count - 1); onSubstract()} : () => alert("No puedes descartar más items")}>-</button>
+      <input disabled type="number" className="item-counter" value={count} aria-label="(Item Count)"/>
+      <button className="btn btn-outline-info" type="button" onClick={(stock > count) ? () => {setCount(count+1); onAdd()} : () => {alert("No contamos con más cantidad de este producto en el momento")}}>+</button>
+    </div> 
+  )
 }
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -3,12 +3,14 @@ import './ItemListContainer.css'
 import { ItemCount } from '../ItemCount/ItemCount.js';
 
 export function ItemListContainer (props){
+    const onAdd = () => {alert("1 (un) item fué añadido al carrito exitosamente")}
+    const onSubstract = () => {alert("1 (un) item fué eliminado del carrito exitosamente")}
     return(
         <div className="card">
             <div className="card-body">
                 <h5 className="card-tittle">{props.tittle}</h5>
                 <p className="card-text">{props.message}</p>
-                <ItemCount stock="4"></ItemCount>
+                <ItemCount stock="4" onAdd={() => onAdd()} onSubstract={() => onSubstract()}></ItemCount>
             </div>
         </div>
     )
