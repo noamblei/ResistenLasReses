de984a39a93611092136d7fb87dc7dec0b3e260c
Noam <noamblei@gmail.com>
2021-06-14 00:56:29 -0300

    Typo fixed

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -6,7 +6,7 @@ export function ItemCount (props) {
     return(
         <div className="input-group mb-3">
           <button className="btn btn-outline-info" type="button" onClick={() => count > 0 ? setCount(count - 1) : alert("No hay más items para descartar")}>-</button>
-          <input disabled type="number" className="item-counter" value={count} aria-label="Itmes count"/>
+          <input disabled type="number" className="item-counter" value={count} aria-label="(Item Count)"/>
           <button className="btn btn-outline-info" type="button" onClick={() => (props.stock > count) ? setCount(count + 1) : alert("No contamos con más cantidad de este producto en el momento")}>+</button>
         </div> 
     )
