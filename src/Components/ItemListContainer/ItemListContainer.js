64b2dc0febd60b7e66ae221f27ba11229928a52b
Noam <noamblei@gmail.com>
2021-06-16 22:14:45 -0300

    promise implemented

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './ItemListContainer.css'
 import { ItemCount } from '../ItemCount/ItemCount.js';
-import { Item } from '../Item/Item.js';
-import { ItemList } from '../ItemList/ItemList.js';
+import Item from '../Item/Item.js';
+import ItemList from '../ItemList/ItemList.js';
 
 export function ItemListContainer (props){
     const onAdd = () => {alert("1 (un) item fué añadido al carrito exitosamente")}
     const onSubstract = () => {alert("1 (un) item fué eliminado del carrito exitosamente")}
+    const [items,setItems] = useState([]);
+    const time = 2000;
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([
+                {
+                    "name": "Dulce de Leche de Almendras", "img": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.k9S8RDfe4AohoTNOecs-MgHaEJ%26pid%3DApi&f=1"
+                }]);
+        }, time)
+    });
+
+    promise.then(result => {
+        setItems(result);
+    });
     return(
         <div className="card">
             <div className="card-body">
